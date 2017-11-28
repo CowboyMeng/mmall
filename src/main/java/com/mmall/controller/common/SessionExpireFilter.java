b612b4396779c3eba924b1d1abb73e49368e40f4
ChouMeng <choumeng@ChouMengdeMacBook-Pro.local>
2017-11-28 21:24:29 +0800

    commit reset session time

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,47 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author: Chou_meng
+ * @Date: 2017/11/28
+ */
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+
+        // 判断loginToken是否为空或者""，如果不为空的话，符合条件，继续拿user信息
+        if (StringUtils.isNotEmpty(loginToken)) {
+            String userJson = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJson, User.class);
+            if (user != null) {
+                // 如果user不为空，则重置session的时间，即调用expire命令
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
